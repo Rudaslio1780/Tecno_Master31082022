c1724b56a3b733db821a9acddffda0d5add5b7cb
LuisPec0 <luisperezcotes@gmail.com>
2022-08-19 20:14:14 -0500

    Creación de proyecto

---
+++ Analisysis_Code/src/main/java/com/tecnomaster/Analisysis_Code/AnalisysisCodeApplication.java
@@ -0,0 +1,13 @@
+package com.tecnomaster.Analisysis_Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisysisCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisysisCodeApplication.class, args);
+	}
+
+}
---
+++ Analisysis_Code/src/test/java/com/tecnomaster/Analisysis_Code/AnalisysisCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnomaster.Analisysis_Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisysisCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
