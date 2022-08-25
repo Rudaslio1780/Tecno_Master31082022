ab6e5bf361ecb8d2e9d6c74f9e2da4e8f6b9b574
rudaslio17 <rudaslio172@gmail.com>
2022-08-25 18:02:08 -0500

    Correccion Clase empleados

--- Analisysis_Code/src/main/java/Empleado.java
+++ Analisysis_Code/src/main/java/Empleado.java
@@ -1,18 +1,21 @@
-public class Empleado  extends  Empresa {
+public class Empleado {
     private  int id;
     private  String nombreEmpleado;
     private  String correo;
     private  String role;
+    private Empresa  empresa;
 
-    public Empleado(long nit, String nombre, String direccion, int telefono, int id, String nombreEmpleado, String correo, String role) {
-        super(nit, nombre, direccion, telefono);
+    public Empleado(int id, String nombreEmpleado, String correo, String role, Empresa empresa)
+    {
         this.id = id;
         this.nombreEmpleado = nombreEmpleado;
         this.correo = correo;
+        this.empresa = empresa;
 
-        if (role.equals("administrador") || role.equals("operativo") )
+        if (role.equalsIgnoreCase("administrador") || role.equalsIgnoreCase("operativo") )
         {
-            this.role = role;
+            String  roleEmpleado = new String(role);
+            this.role= roleEmpleado.toLowerCase();
 
         }
         else
@@ -20,40 +23,62 @@ public class Empleado  extends  Empresa {
             this.role = "operativo";
 
         }
-
     }
 
-
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
 
     public void setNombreEmpleado(String nombreEmpleado) {
         this.nombreEmpleado = nombreEmpleado;
     }
 
-
+    public String getCorreo() {
+        return correo;
+    }
 
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
-   
+    public String getRole() {
+        return role;
+    }
 
     public void setRole(String role) {
-        if (role.equals("administrador") || role.equals("operativo") )
+
+        if (role.equalsIgnoreCase("administrador") || role.equalsIgnoreCase("operativo") )
         {
-            this.role = role;
+
+            String  roleEmpleado = new String(role);
+            this.role= roleEmpleado.toLowerCase();
+
+
 
         }
         else
         {
-            System.out.println("Ingresar operador o Administrador");
+            System.out.println("Ingresar  Operativo o Administrador");
 
         }
 
+    }
+
+    public String getEmpresa() {
+        return empresa.getNombre();
+
+    }
+
+    public void  CambiarNombreEmpresa(String nuevoNombre) {
+
+        this.empresa.setNombre(nuevoNombre);
     }
 }
