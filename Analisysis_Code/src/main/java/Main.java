0f72214676ac5f3a92b60ecd42255a1cbc6a23dc
Juan Sebastian Rodriguez <jsrb48@gmail.com>
2022-08-26 13:10:55 -0500

    Se agregaron pruebas en la clase Main de la clase Empresa

--- Analisysis_Code/src/main/java/Main.java
+++ Analisysis_Code/src/main/java/Main.java
@@ -2,6 +2,33 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //Pruebas Clase Empresa
+        Empresa miEmpresa = new Empresa("Tech Store", "Avenida 28", "3445676", "123456");
+
+        System.out.println("Lectura nombre empresa: "+miEmpresa.getNombre());
+        miEmpresa.setNombre("Tech Store Team");
+        System.out.println("Cambio nombre empresa: "+miEmpresa.getNombre());
+        System.out.println("_____________________");
+
+        System.out.println("Lectura direccion empresa: "+miEmpresa.getDireccion());
+        miEmpresa.setDireccion("Nueva Direccion");
+        System.out.println("Cambio direccion empresa: "+miEmpresa.getDireccion());
+        System.out.println("_____________________");
+
+
+        System.out.println("Lectura telefono empresa: "+miEmpresa.getTelefono());
+        miEmpresa.setTelefono("98765");
+        System.out.println("Cambio telefono empresa: "+miEmpresa.getTelefono());
+        System.out.println("_____________________");
+
+        System.out.println("Lectura nit empresa: "+miEmpresa.getNit());
+        miEmpresa.setNit("898989");
+        System.out.println("Cambio nit empresa: "+miEmpresa.getNit());
+        System.out.println("_____________________");
+
+        //-----------------------------------------------------------------------------------------------------
+
+        //Pruebas Clase Empleado
         Empresa emp1=new Empresa("104542788","Coca cola s.a","Cr 20 30 44","3205412");
         System.out.println("La empresa creada es:"+emp1.getNombre());
         Empleado empleado=new Empleado(3451144,"Jose Rudas","rudaslio17@gmail.com","ADMINISTRADOR",emp1) ;
